5ad4dbdaf1e06d32e3d1329ef5612c42f4f89ed2
Josip-Rizner <rizner.josip@gmail.com>
2021-12-20 17:31:44 +0100

    initialization

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rupap_lv6_zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
